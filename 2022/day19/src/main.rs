93855540c0291bbfcf263a59b73e4b8644b1e1da
Kevin Wang <kevinwang2040@gmail.com>
2023-12-07 11:20:35 -0800

    .

--- 2022/day19/src/main.rs
+++ 2022/day19/src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::collections::VecDeque;
+
 use std::{env, fs};
 
 #[derive(Debug)]
@@ -29,15 +29,17 @@ impl Blueprint {
     }
 }
 
-#[derive(Clone, Copy, Eq, PartialEq, Hash)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 struct State {
     ore_amt: i32,
     clay_amt: i32,
     obs_amt: i32,
+    geo_amt: i32,
     ore_rbt: i32,
     clay_rbt: i32,
     obs_rbt: i32,
     geo_rbt: i32,
+    time: i32,
 }
 
 impl State {
@@ -46,59 +48,68 @@ impl State {
             ore_amt: 0,
             clay_amt: 0,
             obs_amt: 0,
+            geo_amt: 0,
             ore_rbt: 1,
             clay_rbt: 0,
             obs_rbt: 0,
             geo_rbt: 0,
+            time: 1,
         }
     }
 }
 
 fn rec(bp: &Blueprint, seen: &mut HashMap<State, i32>, s: State) -> i32 {
+    // if s.time == 18 {
+    //     println!("{:?}", s);
+    // }
+    if s.time == 25 {
+        return s.geo_amt;
+    }
     if seen.contains_key(&s) {
         return seen[&s];
     }
     let mut options = vec![];
-    if s.ore_amt >= bp.ore_robot_cost {
-        options.push("ore");
-    }
-    if s.clay_amt >= bp.clay_robot_cost {
-        options.push("clay");
+    if s.ore_amt >= bp.geode_robot_cost.0 && s.obs_amt >= bp.geode_robot_cost.1 {
+        options.push("geode");
     }
     if s.ore_amt >= bp.obsidian_robot_cost.0 && s.clay_amt >= bp.obsidian_robot_cost.1 {
         options.push("obs");
     }
-    if s.ore_amt >= bp.geode_robot_cost.0 && s.obs_amt >= bp.geode_robot_cost.1 {
-        options.push("geode");
+    if s.ore_amt >= bp.clay_robot_cost {
+        options.push("clay");
+    }
+    if s.ore_amt >= bp.ore_robot_cost {
+        options.push("ore");
     }
     if options.len() < 4 {
         options.push("wait");
     }
+    // println!("{:?}", options);
     let mut ret = 0;
     for option in options {
         let mut news = s;
         match option {
-            "ore" => {
-                news.ore_amt -= bp.ore_robot_cost;
-                news.ore_rbt += 1;
-            }
-            "clay" => {
-                news.clay_amt -= bp.clay_robot_cost;
-                news.clay_rbt += 1;
-            }
-            "obs" => {
-                news.obs_amt -= bp.obsidian_robot_cost.0;
-                news.clay_amt -= bp.obsidian_robot_cost.1;
-                news.obs_rbt += 1;
-            }
             "geode" => {
                 news.ore_amt -= bp.geode_robot_cost.0;
                 news.obs_amt -= bp.geode_robot_cost.1;
                 news.geo_rbt += 1;
-            }
+            },
+            "obs" => {
+                news.ore_amt -= bp.obsidian_robot_cost.0;
+                news.clay_amt -= bp.obsidian_robot_cost.1;
+                news.obs_rbt += 1;
+            },
+            "clay" => {
+                news.ore_amt -= bp.clay_robot_cost;
+                news.clay_rbt += 1;
+            },
+            "ore" => {
+                news.ore_amt -= bp.ore_robot_cost;
+                news.ore_rbt += 1;
+            },
             "wait" => {
                 // don't make more robots
-            }
+            },
             _ => {
                 panic!("Invalid option");
             }
@@ -108,6 +119,8 @@ fn rec(bp: &Blueprint, seen: &mut HashMap<State, i32>, s: State) -> i32 {
         news.ore_amt += s.ore_rbt;
         news.clay_amt += s.clay_rbt;
         news.obs_amt += s.obs_rbt;
+        news.geo_amt += s.geo_rbt;
+        news.time += 1;
 
         ret = ret.max(rec(bp, seen, news));
     }
@@ -129,8 +142,7 @@ fn main() {
             .unwrap();
         let nums = line[1]
             .split_whitespace()
-            .filter(|w| w.parse::<i32>().is_ok())
-            .map(|w| w.parse::<i32>().unwrap())
+            .filter_map(|w| w.parse::<i32>().ok())
             .collect::<Vec<i32>>();
         blueprints.push(Blueprint::new(
             id,
@@ -141,9 +153,9 @@ fn main() {
         ));
     }
 
-    let initial_state = State::start();
-    let mut seen = HashMap::new();
     for bp in blueprints {
+        let initial_state = State::start();
+        let mut seen = HashMap::new();
         let ans = rec(&bp, &mut seen, initial_state);
         println!("max {}", ans);
     }
