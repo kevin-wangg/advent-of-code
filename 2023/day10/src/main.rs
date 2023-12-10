e9190df1ab29793664bc1450c29845d680c0b719
Kevin Wang <kevinwang2040@gmail.com>
2023-12-09 23:55:37 -0800

    cleanup

--- 2023/day10/src/main.rs
+++ 2023/day10/src/main.rs
@@ -1,11 +1,21 @@
-use std::{env, fs};
 use std::collections::VecDeque;
+use std::{env, fs};
 
 enum Move {
-    Up, Down, Left, Right
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
-fn valid_move(m: Move, y: usize, x: usize, grid: &Vec<Vec<char>>, prev_y: usize, prev_x: usize) -> bool {
+fn valid_move(
+    m: Move,
+    y: usize,
+    x: usize,
+    grid: &Vec<Vec<char>>,
+    prev_y: usize,
+    prev_x: usize,
+) -> bool {
     match m {
         Move::Up => {
             let allowed = vec!['|', 'F', '7'];
@@ -20,7 +30,7 @@ fn valid_move(m: Move, y: usize, x: usize, grid: &Vec<Vec<char>>, prev_y: usize,
                     }
                 }
             }
-        },
+        }
         Move::Down => {
             let allowed = vec!['|', 'L', 'J'];
             if y < grid.len() {
@@ -34,7 +44,7 @@ fn valid_move(m: Move, y: usize, x: usize, grid: &Vec<Vec<char>>, prev_y: usize,
                     }
                 }
             }
-        },
+        }
         Move::Left => {
             let allowed = vec!['-', 'L', 'F'];
             if x > 0 {
@@ -48,7 +58,7 @@ fn valid_move(m: Move, y: usize, x: usize, grid: &Vec<Vec<char>>, prev_y: usize,
                     }
                 }
             }
-        },
+        }
         Move::Right => {
             let allowed = vec!['-', 'J', '7'];
             if x < grid[0].len() {
@@ -82,15 +92,15 @@ fn mark_loop(start_y: usize, start_x: usize, grid: &Vec<Vec<char>>) -> Option<Ve
         match grid[y][x] {
             '|' => {
                 if valid_move(Move::Up, y, x, &grid, prev_y, prev_x) {
-                    marked[2 * y - 1 + 2][ 2 * x + 2] = true;
+                    marked[2 * y - 1 + 2][2 * x + 2] = true;
                     y -= 1;
                 } else if valid_move(Move::Down, y, x, &grid, prev_y, prev_x) {
-                    marked[2 * y + 1 + 2][ 2 * x + 2] = true;
+                    marked[2 * y + 1 + 2][2 * x + 2] = true;
                     y += 1;
                 } else {
                     return None;
                 }
-            },
+            }
             '-' => {
                 if valid_move(Move::Left, y, x, &grid, prev_y, prev_x) {
                     marked[2 * y + 2][2 * x - 1 + 2] = true;
@@ -101,52 +111,52 @@ fn mark_loop(start_y: usize, start_x: usize, grid: &Vec<Vec<char>>) -> Option<Ve
                 } else {
                     return None;
                 }
-            },
+            }
             'F' => {
                 if valid_move(Move::Down, y, x, &grid, prev_y, prev_x) {
-                    marked[2 * y + 1 + 2][ 2 * x + 2] = true;
+                    marked[2 * y + 1 + 2][2 * x + 2] = true;
                     y += 1;
                 } else if valid_move(Move::Right, y, x, &grid, prev_y, prev_x) {
-                    marked[2 * y + 2][ 2 * x + 1 + 2] = true;
+                    marked[2 * y + 2][2 * x + 1 + 2] = true;
                     x += 1;
                 } else {
                     return None;
                 }
-            },
+            }
             'J' => {
                 if valid_move(Move::Up, y, x, &grid, prev_y, prev_x) {
-                    marked[2 * y - 1 + 2][ 2 * x + 2] = true;
+                    marked[2 * y - 1 + 2][2 * x + 2] = true;
                     y -= 1;
                 } else if valid_move(Move::Left, y, x, &grid, prev_y, prev_x) {
-                    marked[2 * y + 2][ 2 * x - 1 + 2] = true;
+                    marked[2 * y + 2][2 * x - 1 + 2] = true;
                     x -= 1;
                 } else {
                     return None;
                 }
-            },
+            }
             'L' => {
                 if valid_move(Move::Up, y, x, &grid, prev_y, prev_x) {
-                    marked[2 * y - 1 + 2][ 2 * x + 2] = true;
+                    marked[2 * y - 1 + 2][2 * x + 2] = true;
                     y -= 1;
                 } else if valid_move(Move::Right, y, x, &grid, prev_y, prev_x) {
-                    marked[2 * y + 2][ 2 * x + 1 + 2] = true;
+                    marked[2 * y + 2][2 * x + 1 + 2] = true;
                     x += 1;
                 } else {
                     return None;
                 }
-            },
+            }
             '7' => {
                 if valid_move(Move::Down, y, x, &grid, prev_y, prev_x) {
-                    marked[2 * y + 1 + 2][ 2 * x + 2] = true;
+                    marked[2 * y + 1 + 2][2 * x + 2] = true;
                     y += 1;
                 } else if valid_move(Move::Left, y, x, &grid, prev_y, prev_x) {
-                    marked[2 * y + 2][ 2 * x - 1 + 2] = true;
+                    marked[2 * y + 2][2 * x - 1 + 2] = true;
                     x -= 1;
                 } else {
                     return None;
                 }
-            },
-            _ => panic!("Invalid character")
+            }
+            _ => panic!("Invalid character"),
         }
         if y == start_y && x == start_x {
             break;
@@ -160,7 +170,7 @@ fn mark_loop(start_y: usize, start_x: usize, grid: &Vec<Vec<char>>) -> Option<Ve
 fn count_outside(mut marked: Vec<Vec<bool>>) -> u32 {
     let mut bfs: VecDeque<(usize, usize)> = VecDeque::new();
     // assume 0,0 is outside
-    marked[2][2] = true;
+    marked[0][0] = true;
     bfs.push_back((0, 0));
     while let Some((y, x)) = bfs.pop_front() {
         if y > 0 {
@@ -201,6 +211,7 @@ fn count_outside(mut marked: Vec<Vec<bool>>) -> u32 {
     inside
 }
 
+#[allow(dead_code)]
 fn print_marked(marked: &Vec<Vec<bool>>) {
     for m in marked {
         for c in m {
@@ -217,9 +228,7 @@ fn print_marked(marked: &Vec<Vec<bool>>) {
 fn main() {
     let path = env::args().nth(1).unwrap_or("input.txt".to_string());
     let input = fs::read_to_string(path).expect("File should exist");
-    let mut grid: Vec<Vec<char>> = input.lines().map(|l| {
-        l.chars().collect()
-    }).collect();
+    let mut grid: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
     let mut y = 0;
     let mut x = 0;
     'outer: for i in 0..grid.len() {
@@ -238,7 +247,7 @@ fn main() {
             Some(marked) => {
                 println!("ans {}", count_outside(marked));
                 break;
-            },
+            }
             None => {}
         }
     }
