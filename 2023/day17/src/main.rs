907fc47690302fed937408f2ae90f51a34ffa39e
Kevin Wang <kevinwang2040@gmail.com>
2023-12-22 22:09:13 -0500

    day 17 bad

--- 2023/day17/src/main.rs
+++ 2023/day17/src/main.rs
@@ -9,52 +9,54 @@ fn main() {
     }).collect();
     let m = grid.len();
     let n = grid[0].len();
-    let mut min_loss = vec![vec![vec![vec![vec![vec![1e9 as usize; 4]; 4]; 4]; 4]; n]; m];
+    let mut min_loss = vec![vec![vec![vec![vec![vec![1e9 as usize; 11]; 11]; 11]; 11]; n]; m];
     let mut bfs = VecDeque::new();
     bfs.push_back((0, 0, 0, 0, 0, 0));
     min_loss[0][0][0][0][0][0] = 0;
     while let Some((y, x, up_moves, right_moves, down_moves, left_moves)) = bfs.pop_front() {
         let cur_loss = min_loss[y][x][up_moves][right_moves][down_moves][left_moves];
-        // println!("cur loss {}", cur_loss);
-        if y > 0 {
-            if up_moves < 3 && down_moves == 0 {
-                if cur_loss + grid[y - 1][x] < min_loss[y - 1][x][up_moves + 1][0][0][0] {
-                    min_loss[y - 1][x][up_moves + 1][0][0][0] = cur_loss + grid[y - 1][x];
-                    bfs.push_back((y - 1, x, up_moves + 1, 0, 0, 0));
-                }
+        if up_moves < 10 && down_moves == 0 {
+            if y > 0 && up_moves > 0 {
+                min_loss[y - 1][x][up_moves + 1][0][0][0] = cur_loss + grid[y - 1][x];
+                bfs.push_back((y - 1, x, up_moves + 1, 0, 0, 0));
+            } else if y > 3 && cur_loss + grid[y - 4][x] + grid[y - 3][x] + grid[y - 2][x] + grid[y - 1][x] < min_loss[y - 4][x][up_moves + 4][0][0][0] {
+                min_loss[y - 4][x][up_moves + 4][0][0][0] = cur_loss + grid[y - 4][x] + grid[y - 3][x] + grid[y - 2][x] + grid[y - 1][x];
+                bfs.push_back((y - 4, x, up_moves + 4, 0, 0, 0));
             }
         }
-        if y < m - 1 {
-            if down_moves < 3 && up_moves == 0 {
-                if cur_loss + grid[y + 1][x] < min_loss[y + 1][x][0][0][down_moves + 1][0] {
-                    min_loss[y + 1][x][0][0][down_moves + 1][0] = cur_loss + grid[y + 1][x];
-                    bfs.push_back((y + 1, x, 0, 0, down_moves + 1, 0));
-                }
+        if down_moves < 10 && up_moves == 0 {
+            if y < m - 1 && down_moves > 0 {
+                min_loss[y + 1][x][0][0][down_moves + 1][0] = cur_loss + grid[y + 1][x];
+                bfs.push_back((y + 1, x, 0, 0, down_moves + 1, 0));
+            } else if y < m - 4 && cur_loss + grid[y + 4][x] + grid[y + 3][x] + grid[y + 2][x] + grid[y + 1][x] < min_loss[y + 4][x][0][0][down_moves + 4][0] {
+                min_loss[y + 4][x][0][0][down_moves + 4][0] = cur_loss + grid[y + 4][x] + grid[y + 3][x] + grid[y + 2][x] + grid[y + 1][x];
+                bfs.push_back((y + 4, x, 0, 0, down_moves + 4, 0));
             }
         }
-        if x > 0 {
-            if left_moves < 3 && right_moves == 0 {
-                if cur_loss + grid[y][x - 1] < min_loss[y][x - 1][0][0][0][left_moves + 1] {
-                    min_loss[y][x - 1][0][0][0][left_moves + 1] = cur_loss + grid[y][x - 1];
-                    bfs.push_back((y, x - 1, 0, 0, 0, left_moves + 1));
-                }
+        if left_moves < 10 && right_moves == 0 {
+            if x > 0 && left_moves > 0 {
+                min_loss[y][x - 1][0][0][0][left_moves + 1] = cur_loss + grid[y][x - 1];
+                bfs.push_back((y, x - 1, 0, 0, 0, left_moves + 1));
+            } else if x > 3 && cur_loss + grid[y][x - 4] + grid[y][x - 3] + grid[y][x - 2] + grid[y][x - 1] < min_loss[y][x - 4][0][0][0][left_moves + 4] {
+                min_loss[y][x - 4][0][0][0][left_moves + 4] = cur_loss + grid[y][x - 4] + grid[y][x - 3] + grid[y][x - 2] + grid[y][x - 1];
+                bfs.push_back((y, x - 4, 0, 0, 0, left_moves + 4));
             }
         }
-        if x < n - 1 {
-            if right_moves < 3 && left_moves == 0 {
-                if cur_loss + grid[y][x + 1] < min_loss[y][x + 1][0][right_moves + 1][0][0] {
-                    min_loss[y][x + 1][0][right_moves + 1][0][0] = cur_loss + grid[y][x + 1];
-                    bfs.push_back((y, x + 1, 0, right_moves + 1, 0, 0));
-                }
+        if right_moves < 10 && left_moves == 0 {
+            if x < n - 1 && right_moves > 0 {
+                min_loss[y][x + 1][0][right_moves + 1][0][0] = cur_loss + grid[y][x + 1];
+                bfs.push_back((y, x + 1, 0, right_moves + 1, 0, 0));
+            } else if x < n - 4 && cur_loss + grid[y][x + 4] + grid[y][x + 3] + grid[y][x + 2] + grid[y][x + 1] < min_loss[y][x + 4][0][right_moves + 4][0][0] {
+                min_loss[y][x + 4][0][right_moves + 4][0][0] = cur_loss + grid[y][x + 4] + grid[y][x + 3] + grid[y][x + 2] + grid[y][x + 1];
+                bfs.push_back((y, x + 4, 0, right_moves + 4, 0, 0));
             }
         }
     }
     let mut ans = 1e9 as usize;
-    for i in 0..4 {
-        for j in 0..4 {
-            for k in 0..4 {
-                for l in 0..4 {
-                    println!("{} {} {} {} {}", i, j, k, l, min_loss[m - 1][n - 1][i][j][k][l]);
+    for i in 0..11 {
+        for j in 0..11 {
+            for k in 0..11 {
+                for l in 0..11 {
                     ans = ans.min(min_loss[m - 1][n - 1][i][j][k][l]);
                 }
             }
