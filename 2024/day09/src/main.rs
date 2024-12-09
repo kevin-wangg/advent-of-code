668ba1e0d19f752a5cecdc45a72dd84039860fa6
Kevin Wang <kevinwang2040@gmail.com>
2024-12-09 02:10:43 -0800

    day 09 part 2

--- 2024/day09/src/main.rs
+++ 2024/day09/src/main.rs
@@ -75,7 +75,7 @@ fn part1(input: &str) {
         match block.file {
             BlockType::File(id) => {
                 for _j in 0..block.length {
-                    ans += i * id; 
+                    ans += i * id;
                     i += 1;
                 }
             }
@@ -87,8 +87,86 @@ fn part1(input: &str) {
     println!("ans {ans}");
 }
 
+fn move_block(id: u64, blocks: &mut Vec<Block>) {
+    let mut ind = 0;
+    for i in (0..blocks.len()).rev() {
+        match blocks[i].file {
+            BlockType::File(idx) => {
+                if id == idx {
+                    ind = i;
+                    break;
+                }
+            }
+            BlockType::Empty => {}
+        }
+    }
+    let length = blocks[ind].length;
+    for i in 0..ind {
+        match blocks[i].file {
+            BlockType::Empty => {
+                if blocks[i].length >= length {
+                    let rem = blocks[i].length - length;
+                    blocks[i].length = rem;
+                    blocks[ind].file = BlockType::Empty;
+                    blocks.insert(
+                        i,
+                        Block {
+                            file: BlockType::File(id),
+                            length,
+                        },
+                    );
+                    break;
+                }
+            }
+            BlockType::File(_) => {}
+        }
+    }
+}
+
+fn part2(input: &str) {
+    let mut file = true;
+    let mut blocks = Vec::new();
+    let mut id = 0;
+    for c in input.trim().chars() {
+        let digit = c.to_digit(10).unwrap();
+        blocks.push(Block {
+            file: if file {
+                BlockType::File(id)
+            } else {
+                BlockType::Empty
+            },
+            length: digit as u64,
+        });
+        if file {
+            id += 1;
+        }
+        file = !file;
+    }
+    for i in (0..id).rev() {
+        move_block(i, &mut blocks);
+    }
+
+    let mut ans = 0;
+    let mut i = 0;
+    for block in blocks {
+        match block.file {
+            BlockType::File(id) => {
+                for _j in 0..block.length {
+                    ans += i * id;
+                    i += 1;
+                }
+            }
+            BlockType::Empty => {
+                i += block.length;
+            }
+        }
+    }
+    println!("ans {ans}");
+}
+
 fn main() {
     let path = env::args().nth(1).unwrap_or("input.txt".to_string());
     let input = fs::read_to_string(path).expect("File should exist");
     part1(&input);
+    part2(&input);
 }
