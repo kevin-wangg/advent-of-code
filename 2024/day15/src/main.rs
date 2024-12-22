8b907e6b14bcec9037507f13694779d2a36198cb
Kevin Wang <kevinwang2040@gmail.com>
2024-12-21 22:47:50 -0800

    day 15 p2

--- 2024/day15/src/main.rs
+++ 2024/day15/src/main.rs
@@ -3,10 +3,12 @@ use std::fs;
 
 fn solve(input: &str) {
     let inputs: Vec<&str> = input.split("\n\n").collect();
-    let mut grid: Vec<Vec<char>> = inputs[0]
+    let grid: Vec<Vec<char>> = inputs[0]
         .lines()
         .map(|line| line.chars().collect())
         .collect();
+
+    let mut grid = expand_grid(grid);
 
     let moves = inputs[1];
     let mut start = (0, 0);
@@ -20,15 +22,42 @@ fn solve(input: &str) {
     }
     for c in moves.chars() {
         if !c.is_whitespace() {
-            start = do_move(start.0, start.1, &mut grid, c);
+            start = process_move(start.0, start.1, &mut grid, c);
         }
     }
 
-    print(&grid);
     println!("ans {}", score(&grid));
 }
 
-fn check_move(y: usize, x: usize, grid: &mut Vec<Vec<char>>, c: char) -> bool {
+fn expand_grid(grid: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut ret = vec![Vec::new(); grid.len()];
+    for i in 0..grid.len() {
+        for c in &grid[i] {
+            match c {
+                '#' => {
+                    ret[i].push('#');
+                    ret[i].push('#');
+                }
+                'O' => {
+                    ret[i].push('[');
+                    ret[i].push(']');
+                }
+                '.' => {
+                    ret[i].push('.');
+                    ret[i].push('.');
+                }
+                '@' => {
+                    ret[i].push('@');
+                    ret[i].push('.');
+                }
+                _ => panic!("invalid character {}", c),
+            }
+        }
+    }
+    ret
+}
+
+fn check_move(y: usize, x: usize, grid: &mut Vec<Vec<char>>, c: char, check_beside: bool) -> bool {
     if grid[y][x] == '.' {
         true
     } else if grid[y][x] == '#' {
@@ -41,17 +70,52 @@ fn check_move(y: usize, x: usize, grid: &mut Vec<Vec<char>>, c: char) -> bool {
             'v' => (y + 1, x),
             _ => panic!("invalid move {}", c),
         };
-        if check_move(next.0, next.1, grid, c) {
-            grid[next.0][next.1] = grid[y][x];
-            true
-        } else {
-            false
+        let mut ok = check_move(next.0, next.1, grid, c, true);
+        if (c == '^' || c == 'v') && grid[y][x] != '@' && check_beside {
+            if grid[y][x] == '[' {
+                ok &= check_move(y, x + 1, grid, c, false);
+            } else if grid[y][x] == ']' {
+                ok &= check_move(y, x - 1, grid, c, false);
+            } else {
+                panic!("invalid box character {}", grid[y][x]);
+            }
         }
+        ok
     }
 }
 
-fn do_move(y: usize, x: usize, grid: &mut Vec<Vec<char>>, c: char) -> (usize, usize) {
-    if check_move(y, x, grid, c) {
+fn do_move(y: usize, x: usize, grid: &mut Vec<Vec<char>>, c: char, check_beside: bool) {
+    if grid[y][x] == '.' {
+        return;
+    } else if grid[y][x] == '#' {
+        return;
+    } else {
+        let next = match c {
+            '>' => (y, x + 1),
+            '<' => (y, x - 1),
+            '^' => (y - 1, x),
+            'v' => (y + 1, x),
+            _ => panic!("invalid move {}", c),
+        };
+        do_move(next.0, next.1, grid, c, true);
+        if (c == '^' || c == 'v') && grid[y][x] != '@' && check_beside {
+            if grid[y][x] == '[' {
+                do_move(y, x + 1, grid, c, false);
+                grid[y][x + 1] = '.';
+            } else if grid[y][x] == ']' {
+                do_move(y, x - 1, grid, c, false);
+                grid[y][x - 1] = '.';
+            } else {
+                panic!("invalid box character {}", grid[y][x]);
+            }
+        }
+        grid[next.0][next.1] = grid[y][x];
+    }
+}
+
+fn process_move(y: usize, x: usize, grid: &mut Vec<Vec<char>>, c: char) -> (usize, usize) {
+    if check_move(y, x, grid, c, true) {
+        do_move(y, x, grid, c, true);
         grid[y][x] = '.';
         match c {
             '>' => (y, x + 1),
@@ -69,7 +133,7 @@ fn score(grid: &[Vec<char>]) -> i32 {
     let mut ret = 0;
     for i in 0..grid.len() {
         for j in 0..grid[i].len() {
-            if grid[i][j] == 'O' {
+            if grid[i][j] == '[' {
                 ret += 100 * i as i32 + j as i32;
             }
         }
@@ -77,6 +141,7 @@ fn score(grid: &[Vec<char>]) -> i32 {
     ret
 }
 
+#[allow(dead_code)]
 fn print(grid: &[Vec<char>]) {
     for i in 0..grid.len() {
         for j in 0..grid[i].len() {
