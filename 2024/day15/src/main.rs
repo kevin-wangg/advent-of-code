259a7621482f87ac6028f38093972a8598b3930a
Kevin Wang <kevinwang2040@gmail.com>
2024-12-21 16:14:01 -0800

    day 15 p1

--- 2024/day15/src/main.rs
+++ 2024/day15/src/main.rs
@@ -1,43 +1,89 @@
 use std::env;
-
 use std::fs;
-
-fn do_move(y: usize, x: usize, grid: &mut Vec<Vec<char>>, c: char) -> (usize, usize) {
-    match c {
-        '>' => {
-            let mut cnt = 1;
-            loop {
-                if grid[y][x + cnt] == '#' {
-                    return (y, x);
-                }
-                if grid[y][x + cnt] == '.' {
-                    break;
-                }
-                cnt += 1;
-            }
-            let end = x + cnt;
-            for i in (x..end).rev() {
-                grid[y][i + 1] = grid[y][i];
-            }
-            grid[y][x] = '.';
-            (y, x + 1)
-        }
-        '<' => {}
-        '^' => {}
-        'v' => {}
-        _ => {}
-    }
-}
 
 fn solve(input: &str) {
     let inputs: Vec<&str> = input.split("\n\n").collect();
-    let grid: Vec<Vec<char>> = inputs[0]
+    let mut grid: Vec<Vec<char>> = inputs[0]
         .lines()
         .map(|line| line.chars().collect())
         .collect();
 
-    // Remember to ignore newlines
     let moves = inputs[1];
+    let mut start = (0, 0);
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            if grid[i][j] == '@' {
+                start = (i, j);
+                break;
+            }
+        }
+    }
+    for c in moves.chars() {
+        if !c.is_whitespace() {
+            start = do_move(start.0, start.1, &mut grid, c);
+        }
+    }
+
+    print(&grid);
+    println!("ans {}", score(&grid));
+}
+
+fn check_move(y: usize, x: usize, grid: &mut Vec<Vec<char>>, c: char) -> bool {
+    if grid[y][x] == '.' {
+        true
+    } else if grid[y][x] == '#' {
+        false
+    } else {
+        let next = match c {
+            '>' => (y, x + 1),
+            '<' => (y, x - 1),
+            '^' => (y - 1, x),
+            'v' => (y + 1, x),
+            _ => panic!("invalid move {}", c),
+        };
+        if check_move(next.0, next.1, grid, c) {
+            grid[next.0][next.1] = grid[y][x];
+            true
+        } else {
+            false
+        }
+    }
+}
+
+fn do_move(y: usize, x: usize, grid: &mut Vec<Vec<char>>, c: char) -> (usize, usize) {
+    if check_move(y, x, grid, c) {
+        grid[y][x] = '.';
+        match c {
+            '>' => (y, x + 1),
+            '<' => (y, x - 1),
+            '^' => (y - 1, x),
+            'v' => (y + 1, x),
+            _ => panic!("invalid move {}", c),
+        }
+    } else {
+        (y, x)
+    }
+}
+
+fn score(grid: &[Vec<char>]) -> i32 {
+    let mut ret = 0;
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            if grid[i][j] == 'O' {
+                ret += 100 * i as i32 + j as i32;
+            }
+        }
+    }
+    ret
+}
+
+fn print(grid: &[Vec<char>]) {
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            print!("{}", grid[i][j]);
+        }
+        println!();
+    }
 }
 
 fn main() {
