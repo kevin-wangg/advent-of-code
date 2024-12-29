f10930a27b92bb34b8f62992fdd0798fe3640631
Kevin Wang <kevinwang2040@gmail.com>
2024-12-28 20:54:30 -0500

    save

--- 2024/day17/src/main.rs
+++ 2024/day17/src/main.rs
@@ -1,18 +1,19 @@
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 
 struct StateMachine {
-    a: i32,
-    b: i32,
-    c: i32,
+    a: i64,
+    b: i64,
+    c: i64,
     pc: usize,
-    program: Vec<i32>,
-    output: Vec<i32>,
+    program: Vec<i64>,
+    output: Vec<i64>,
 }
 
 impl StateMachine {
+    #[allow(dead_code)]
     fn print_state(&self) {
-        println!("===========");
         println!("Register A: {}", self.a);
         println!("Register B: {}", self.b);
         println!("Register C: {}", self.c);
@@ -23,7 +24,7 @@ impl StateMachine {
         self.pc = pc;
     }
 
-    fn read_instruction(&mut self) -> Option<i32> {
+    fn read_instruction(&mut self) -> Option<i64> {
         if self.pc >= self.program.len() {
             None
         } else {
@@ -33,7 +34,7 @@ impl StateMachine {
         }
     }
 
-    fn combo_operand(&self, operand: i32) -> i32 {
+    fn combo_operand(&self, operand: i64) -> i64 {
         match operand {
             0..=3 => operand,
             4 => self.a,
@@ -45,48 +46,48 @@ impl StateMachine {
         }
     }
 
-    fn literal_operand(&self, operand: i32) -> i32 {
+    fn literal_operand(&self, operand: i64) -> i64 {
         operand
     }
 
-    fn handle_adv(&mut self, operand: i32) {
+    fn handle_adv(&mut self, operand: i64) {
         // Performs division
         let numerator = self.a;
-        let denominator = 2_i32.pow(self.combo_operand(operand) as u32);
+        let denominator = 2_i64.pow(self.combo_operand(operand) as u32);
         self.a = numerator / denominator;
     }
 
-    fn handle_bxl(&mut self, operand: i32) {
+    fn handle_bxl(&mut self, operand: i64) {
         self.b = self.b ^ self.literal_operand(operand);
     }
 
-    fn handle_bst(&mut self, operand: i32) {
+    fn handle_bst(&mut self, operand: i64) {
         self.b = self.combo_operand(operand) % 8;
     }
 
-    fn handle_jnz(&mut self, operand: i32) {
+    fn handle_jnz(&mut self, operand: i64) {
         if self.a != 0 {
             self.set_pc(self.literal_operand(operand) as usize);
         }
     }
 
-    fn handle_bxc(&mut self, _operand: i32) {
+    fn handle_bxc(&mut self, _operand: i64) {
         self.b = self.b ^ self.c;
     }
 
-    fn handle_out(&mut self, operand: i32) {
+    fn handle_out(&mut self, operand: i64) {
         self.output.push(self.combo_operand(operand) % 8);
     }
 
-    fn handle_bdv(&mut self, operand: i32) {
+    fn handle_bdv(&mut self, operand: i64) {
         let numerator = self.a;
-        let denominator = 2_i32.pow(self.combo_operand(operand) as u32);
+        let denominator = 2_i64.pow(self.combo_operand(operand) as u32);
         self.b = numerator / denominator;
     }
 
-    fn handle_cdv(&mut self, operand: i32) {
+    fn handle_cdv(&mut self, operand: i64) {
         let numerator = self.a;
-        let denominator = 2_i32.pow(self.combo_operand(operand) as u32);
+        let denominator = 2_i64.pow(self.combo_operand(operand) as u32);
         self.c = numerator / denominator;
     }
 
@@ -100,8 +101,6 @@ impl StateMachine {
 
         let opcode = opcode.unwrap();
         let operand = operand.unwrap();
-
-        println!("{} {}", opcode, operand);
 
         match opcode {
             0 => {
@@ -137,25 +136,37 @@ impl StateMachine {
     }
 }
 
-fn parse_program(p: &str) -> Vec<i32> {
+fn parse_program(p: &str) -> Vec<i64> {
     let p: Vec<&str> = p.trim().split(": ").collect();
     p[1].split(',').map(|n| n.parse().unwrap()).collect()
 }
 
-fn parse_registers(input: Vec<&str>) -> (i32, i32, i32) {
-    let a: i32 = input[0].split(": ").collect::<Vec<&str>>()[1]
+fn parse_registers(input: Vec<&str>) -> (i64, i64, i64) {
+    let a: i64 = input[0].split(": ").collect::<Vec<&str>>()[1]
         .trim()
         .parse()
         .unwrap();
-    let b: i32 = input[1].split(": ").collect::<Vec<&str>>()[1]
+    let b: i64 = input[1].split(": ").collect::<Vec<&str>>()[1]
         .trim()
         .parse()
         .unwrap();
-    let c: i32 = input[2].split(": ").collect::<Vec<&str>>()[1]
+    let c: i64 = input[2].split(": ").collect::<Vec<&str>>()[1]
         .trim()
         .parse()
         .unwrap();
     (a, b, c)
+}
+
+fn check_nth_output(mut machine: StateMachine, candidate: &str, n: usize) -> bool {
+    let a = i64::from_str_radix(candidate, 2).unwrap();
+    machine.a = a;
+    loop {
+        let res = machine.do_instruction();
+        if res.is_none() {
+            break;
+        }
+    }
+    machine.output[n] == machine.program[n]
 }
 
 fn solve(input: &str) {
@@ -163,26 +174,62 @@ fn solve(input: &str) {
     let registers: Vec<&str> = input[0].lines().collect();
     let program = parse_program(input[1]);
     let registers = parse_registers(registers);
-    println!("{:?} {:?}", registers, program);
+
     let mut machine = StateMachine {
         a: registers.0,
         b: registers.1,
         c: registers.2,
         pc: 0,
-        program,
+        program: program.clone(),
         output: Vec::new(),
     };
 
+    let mut candidate: i64 = 8_i64.pow(15);
+
+    let mut tmp = candidate;
+
+    let mut possible_outputs = HashSet::new();
+
     loop {
-        let res = machine.do_instruction();
-        if res.is_none() {
+        machine.a = candidate;
+        machine.b = registers.1;
+        machine.c = registers.2;
+        machine.pc = 0;
+        machine.output.clear();
+
+        loop {
+            let res = machine.do_instruction();
+            if res.is_none() {
+                break;
+            }
+        }
+
+        possible_outputs.insert(machine.output.clone());
+
+        if machine.output[0] == 2
+            && machine.output[1] == 4
+            && machine.output[2] == 1
+            && machine.output[3] == 1
+            && machine.output[4] == 7
+            && machine.output[5] == 5
+        {
+            println!("===========");
+            println!("candidate: {}", candidate);
+            println!("candidate binary: {:b}", candidate);
+            println!("diff: {}", candidate - tmp);
+            tmp = candidate;
+            machine.print_state();
+            println!("possible outputs size {}", possible_outputs.len());
+        }
+
+        if machine.output == program {
             break;
         }
-        machine.print_state();
+
+        candidate += 1;
     }
 
-    let output = machine.output.iter().map(|n| n.to_string()).collect::<Vec<_>>().join(",");
-    println!("{}", output);
+    println!("ans {}", candidate);
 }
 
 fn main() {
